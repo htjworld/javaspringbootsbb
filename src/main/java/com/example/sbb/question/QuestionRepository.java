4d27dfa8b48afa99533ebff8cb6cbb9afe458ba8
htjworld <itjhan01@gmail.com>
2024-08-14 15:27:59 +0900

    리스트에서 게시글마다 댓글수 표시

--- src/main/java/com/example/sbb/question/QuestionRepository.java
+++ src/main/java/com/example/sbb/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.example.sbb.question;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
