0ac865d66573bc8c5493ca84a8c30e193e64271d
htjworld <itjhan01@gmail.com>
2024-08-14 14:04:00 +0900

    로그인 네비게이션 바까지 구현(기능x)

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import com.example.sbb.answer.Answer;
 import com.example.sbb.question.Question;
 import com.example.sbb.question.QuestionRepository;
+import com.example.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,22 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
-//	@Autowired
-//	private AnswerRepository answerRepository;
-	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = String.format("%d번째 데이터",i);
+			this.questionService.create(subject, content);
 		}
+	}
 }
 
 
